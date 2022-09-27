4bf05767a83fb0c0f542583618250896a94226c5
KarenGalindo <daniiela2654@gmail.com>
2022-09-26 20:11:38 -0500

    BotonCreate mun, pos, dt

---
+++ Torneo.App.Frontend/Pages/DTs/Create.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
+
+namespace Torneo.App.Frontend.PagesDTs
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IRepositorioDT _repoDT;
+        public DirectorTecnico directorTecnico {get; set;}
+
+        public CreateModel(IRepositorioDT repoDT)
+        {
+            _repoDT = repoDT;
+        }
+
+        public void OnGet()
+        {
+            directorTecnico = new DirectorTecnico();
+        }
+
+        public IActionResult OnPost(DirectorTecnico directorTecnico)
+        {
+            _repoDT.AddDT(directorTecnico);
+            return RedirectToPage("Index");
+        }
+    }
+}
